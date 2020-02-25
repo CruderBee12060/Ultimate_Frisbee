473635f5faf007ad37f2355e1fa900699081644e
CruderBee12060 <61299197+CruderBee12060@users.noreply.github.com>
2020-02-25 12:34:34 -0800

    Update Coach.java

--- src/Coach.java
+++ src/Coach.java
@@ -3,9 +3,10 @@ public class Coach extends Person {
     private String role;
 
     public Coach(String firstName, String lastName, String role){
-
+        super(firstName, lastName);
+        this.role = role;
     }
     public String toString(){
-
+        return super toString() + "\n   Role: " + role;
     }
 }
