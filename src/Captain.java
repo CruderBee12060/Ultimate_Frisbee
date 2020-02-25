ede3e96e5cf60b37438798c617a4481d4d114574
CruderBee12060 <61299197+CruderBee12060@users.noreply.github.com>
2020-02-25 12:34:49 -0800

    Update Captain.java

--- src/Captain.java
+++ src/Captain.java
@@ -3,9 +3,16 @@ public class Captain extends UltimatePlayer{
     private boolean type;
 
     public Captain(String firstName, String lastName, String position, boolean type){
-
+    super (firstName, lastName, position);
+    this.type = type;
     }
     public String toString(){
-
+        String t;
+        if(type){
+            t = "offense";
+        }else{
+            t = "defense"
+        }
+        return super.toString() + "\n   Captain: " + t;
     }
 }
