ff15d1f0e31cffc363cb5fecc94410c80f087dc1
CruderBee12060 <61299197+CruderBee12060@users.noreply.github.com>
2020-02-25 12:35:15 -0800

    Create UltimateTeam.java

---
+++ src/UltimateTeam.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class UltimateTeam {
+    private ArrayList<UltimatePlayer> players;
+    private ArrayList<Coach> coaches;
+
+    public UltimateTeam(ArrayList<UltimatePlayer> players, ArrayList<Coach> coaches){
+        this.players = players;
+        this.coaches = coaches;
+    }
+    public String getCutters(){
+        String returned = "";
+        for(int i)
+        return super.
+    }
+    public String getHandlers(){
+        String returned = "";
+        return super.
+    }
+    public String toString(){
+        String returned = "";
+        return "COACHES" + "\n" + coaches + "\nPLAYERS" + players;
+    }
+}
